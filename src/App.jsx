b5709132a8dc63ac737270c2142a54e00bc51306
yurishe0 <yurishe00@gmail.com>
2024-05-18 15:49:41 +0200

    feat: initial site structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Header from './components/Header';
+import Game from './components/Game';
+import Scoreboard from './components/Scoreboard';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      test
+      <Header></Header>
+      <Game>
+        <Scoreboard />
+        <Cards />
+      </Game>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

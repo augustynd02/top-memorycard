2bcd97afe751852683b2ebf93fcf0612fb078149
yurishe0 <yurishe00@gmail.com>
2024-06-03 17:18:29 +0200

    feat: randomize data order after every click

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,8 +12,24 @@ function createCards(imagesData, handleClick) {
     return cards;
 }
 
+function randomizeData(imagesData) {
+    const arr = new Array(imagesData.length).fill(null);
+    for(let i = 0; i < imagesData.length; i++) {
+        let placed = false;
+        while(!placed) {
+            let randomNum = Math.floor(Math.random() * 12);
+            if(arr[randomNum] === null) {
+                arr[randomNum] = imagesData[i]
+                placed = true;
+            }
+        }
+    }
+    return arr;
+}
+
 export default function Cards({imagesData, handleClick}) {
-    const cards = createCards(imagesData, handleClick)
+    const randomizedData = randomizeData(imagesData);
+    const cards = createCards(randomizedData, handleClick)
     return (
         <div className="cards">
             {cards}

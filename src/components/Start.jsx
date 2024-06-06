1c502d2b4956ddc564204e0f574a34b3dc4b06f0
yurishe0 <yurishe00@gmail.com>
2024-06-06 16:00:57 +0200

    style: style the query menu and adjust card styling

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,3 +1,5 @@
+import '../styles/Start.css';
+
 export default function Start({ setIsGameOn, setQuery }) {
     function handleSubmit(e) {
         e.preventDefault();
@@ -6,7 +8,7 @@ export default function Start({ setIsGameOn, setQuery }) {
         setIsGameOn(true);
     }
     return (
-        <div className="main-container">
+        <div className="main-container menu">
             <h2>Select your topic:</h2>
             <input type="text" id="query"/>
             <button onClick={(e) => { handleSubmit(e) }}>OK</button>
